8ee797053497e8fdfcdffeee0d71e48f45e701b1
Joshua Wilding <joshua.wilding@aggiemail.usu.edu>
2015-12-09 13:16:38 -0700

    Added database connection URI to config.js. Created a basic client wrapper

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ indexRoutes	= require('./routes/index.js')
 userRoutes	= require('./routes/user.js')
 session = require('express-session')
 querystring = require('querystring')
-
+client = require('./public/js/db')
 cfg = require('./config.js')
 
 
@@ -32,4 +32,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRoutes)
 app.use('/user', userRoutes)
 
-app.listen(3000)
+client.connect(cfg.db_connect, function(err){
+  if(err){
+    console.log('Error in connecting to Mongo. Please check your database connections')
+    process.exit(1)
+  } else {
+    app.listen(3000)
+  }
+})
--- config.js
+++ config.js
@@ -4,5 +4,6 @@ config.access_token = ''
 config.client_id = 'afdc0b4978f149cab3801f5501116ebd'
 config.client_secret = '341145521ab545beb9ac1eef625cbd7c'
 config.redirect_uri = 'http://127.0.0.1:3000/auth/finalize'
+config.db_connect = 'mongodb://admin:admin@ds043398.mongolab.com:43398/test-database'
 
 module.exports = config
